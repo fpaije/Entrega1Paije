40059df90db4775dcc724a6bcfe7a6e8b26ce3be
FPaije <hfpaije@gmail.com>
2022-02-02 12:09:26 -0300

    Subida clase 21

--- Appcoder/urls.py
+++ Appcoder/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from Appcoder.views import *
 
 urlpatterns = [
-    path('curso/', curso, name="Curso"),
-    path('inicio/', inicio, name="Inicio"),    
+    path('curso/', curso, name="AppcoderCurso"),
+    path('inicio/', inicio, name="AppcoderInicio"),
+    path('cursoformulario/', curso_formulario, name="AppcoderCursoFormulario"),        
 ]
--- Appcoder/views.py
+++ Appcoder/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from datetime import datetime
 
+from Appcoder.forms import CursoFormulario
+from.models import *
 
 # Create your views here.
 
@@ -16,3 +18,21 @@ def curso(request):
 
 def inicio(request):
     return render(request, 'Appcoder/index.html')
+
+def curso_formulario(request):
+
+    if request.method == 'POST':
+    
+        miformulario = CursoFormulario(request.POST)
+
+        #print(miformulario)
+
+        if miformulario.is_valid:
+            info = miformulario.data
+            curso = Curso(nombre= info['nombre'], camada= info['camada'])
+            curso.save()
+
+    miformulario = CursoFormulario()
+    
+    return render(request, 'Appcoder/curso_formulario.html', {"miformulario": miformulario})
+
